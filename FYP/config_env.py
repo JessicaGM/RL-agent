35d8980e76449ee6d99fa7d20ff5edba24895ec0
Jessica <60440955+JessicaGM@users.noreply.github.com>
2024-02-12 18:30:07 +0000

    Update config_env to deal with env creation with the specific configuration, update the policy frequency to 5 to stop the car from spinning (a must for Continuous action, otherwise agent spins in circles to gain the highest reward), as well as updated steering to cap at [-0.5, 0.5], lower down the vehicle count for now, change offroad_terminal to False to make the agent learn staying on the road rather than terminating episode.

--- FYP/config_env.py
+++ FYP/config_env.py
@@ -1,47 +1,68 @@
-def env_config():
-    """
-    Set the configuration for the environment.
+import gymnasium as gym
+import numpy as np
 
-    Returns:
-        dict: A dictionary containing configuration parameters for the environment.
+
+class ConfigEnv:
+    """
+    ConfigEnv class for configuring and creating a highway environment.
 
     The configuration includes settings for observation, action, lanes, vehicles,
     duration, rewards, screen dimensions, and other environmental parameters.
+
+    Attributes:
+        id (str): The ID of the highway environment.
+        config (dict): Configuration parameters for the highway environment.
     """
-    config = {
-        "observation": {
-            "type": "Kinematics",
-            "normalize": False
-        },
-        "action": {
-            "type": "ContinuousAction",
-        },
-        "lanes_count": 5,
-        "vehicles_count": 50,
-        "controlled_vehicles": 1,
-        "initial_lane_id": None,
-        "duration": 40,  # [s]
-        "ego_spacing": 2,
-        "vehicles_density": 1,
-        "collision_reward": -1,  # The reward received when colliding with a vehicle.
-        "right_lane_reward": 0.1,  # The reward received when driving on the right-most lanes, linearly mapped to
-        # zero for other lanes.
-        "high_speed_reward": 0.4,  # The reward received when driving at full speed, linearly mapped to zero for
-        # lower speeds according to config["reward_speed_range"].
-        "lane_change_reward": 0,  # The reward received at each lane change action.
-        "reward_speed_range": [20, 30],
-        "normalize_reward": True,
-        "offroad_terminal": True,
-        "screen_width": 1000,
-        "screen_height": 500,
-        "centering_position": [0.1, 0.5],
-        "longitudinal": True,
-        "lateral": True,
-        "order": "sorted"
-    }
 
-    return config
+    def __init__(self):
+        """
+        Initialize ConfigEnv with default ID and configuration parameters.
+        """
+        self.id = 'highway-v0'
+        self.config = {
+            "observation": {
+                "type": "Kinematics",
+                "normalize": False,
+                "features": ["presence", "x", "y", "vx", "vy", "heading", "cos_h",
+                             "sin_h", "cos_d", "sin_d", "long_off", "lat_off", "ang_off"],
+            },
+            "action": {
+                "type": "ContinuousAction",
+                "steering_range": [-0.5, 0.5],  # updated to stop car spinning
+                "longitudinal": True,
+                "lateral": True,
+            },
+            "policy_frequency": 5,  # updated to stop car spinning (a must for Continuous action)
+            "lanes_count": 5,
+            "vehicles_count": 20,
+            "controlled_vehicles": 1,
+            "initial_lane_id": None,
+            "duration": 40,  # [s]
+            "ego_spacing": 2,
+            "vehicles_density": 1,
+            "collision_reward": -1,  # The reward received when colliding with a vehicle.
+            "right_lane_reward": 0.1,  # The reward received when driving on the right-most lanes, linearly mapped to
+            # zero for other lanes.
+            "high_speed_reward": 0.4,  # The reward received when driving at full speed, linearly mapped to zero for
+            # lower speeds according to config["reward_speed_range"].
+            "lane_change_reward": 0,  # The reward received at each lane change action.
+            "reward_speed_range": [20, 30],
+            "normalize_reward": True,
+            "offroad_terminal": False,  # learn to stay on the road rather than terminate episode
+            "screen_width": 1000,
+            "screen_height": 500,
+            "centering_position": [0.1, 0.5],
+            "order": "sorted"
+        }
 
+    def make_configured_env(self, render_mode=None):
+        """
+        Create and configure a highway environment based on the stored ID and configuration.
 
-class EnvConfig:
-    pass
+        Returns:
+            gym.Env: Configured highway environment.
+        """
+        env = gym.make(self.id, render_mode=render_mode)
+        env.configure(self.config)
+        env.reset()
+        return env
